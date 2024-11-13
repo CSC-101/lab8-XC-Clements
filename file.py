6c838bf72580f681b11f8514de748fb64598d045
Hikatchus <76414030+Hikatchus@users.noreply.github.com>
2024-11-13 11:11:56 -0800

    11/13 10:02

--- file.py
+++ file.py
@@ -3,7 +3,8 @@ import sys
 def convert_to_float(input_str:str):
     try:
         return float(input_str)
-    except:
+    except ValueError as e:
+        print(e)
         return None
 
 
@@ -16,10 +17,8 @@ def can_opener():
         return "error: no argument provided"
     except FileNotFoundError:
         return "error: file not found"
-    final_output_list = []
+
     for line in input_file:
-        output = None
-        temp_output_list = []
         char_list = line.split()
         if len(char_list) == 2:
             if convert_to_float(char_list[0]) and convert_to_float(char_list[1]):
